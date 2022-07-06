36dd3b2513469b5ce3d3ba20179089b3073b35bd
Andrej Jurkin <andrej@jurkin.io>
2022-07-06 16:20:28 +0200

    Change prettier config to use double quote

--- static/template/main.js
+++ static/template/main.js
@@ -1,5 +1,5 @@
-import './style.css';
+import "./style.css";
 
-const hello = 'Hello World!!';
+const hello = "Hello World!!";
 
-document.querySelector('#app').textContent = `${hello}`;
+document.querySelector("#app").textContent = `${hello}`;
