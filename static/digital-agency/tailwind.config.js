3bf69c4235fedd45c533f890289b9e62a942d0a2
Subho <contact.subhoghosh@gmail.com>
2022-07-06 19:55:37 +0530

    Added primary color & font in tailwind config

--- static/digital-agency/tailwind.config.js
+++ static/digital-agency/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2177F2'
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
