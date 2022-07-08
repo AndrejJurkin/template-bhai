63d3e0912c246c408efcf4a9d2a55590282051c3
Subho <contact.subhoghosh@gmail.com>
2022-07-08 16:11:50 +0530

    Feat: implement hamburger menu

--- static/digital-agency/main.js
+++ static/digital-agency/main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+const toggleManuBtn = document.querySelector("#toggle-menu-btn");
+const hamIcon = document.querySelector("#toggle-menu-btn > img:first-child");
+const closeIcon = document.querySelector("#toggle-menu-btn > img:last-child");
+
+const navMenu = document.querySelector("#nav-menu");
+
+const overlay = document.querySelector("#overlay");
+
+toggleManuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("hidden");
+
+  hamIcon.classList.toggle("hidden");
+  closeIcon.classList.toggle("hidden");
+
+  overlay.classList.toggle("hidden");
+});
