c2f239d6911fc692abf535b6735233a844804883
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-09 16:06:01 +0530

    Implemented best practices.

--- static/simple-portfolio-template/tailwind.config.js
+++ static/simple-portfolio-template/tailwind.config.js
@@ -1,13 +1,26 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
+      maxWidth: {
+        "full-width": "1920px",
+      },
+
       backgroundImage: {
         "hero-bg": "url('../assets/images/hero-bg.jpg')",
       },
 
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+      },
+
+      colors: {
+        primary: "#0d0d0d",
+        secondary: "#fff",
+        popping: colors.yellow[400],
+        "sticky-nav": "#353956fa",
       },
 
       gridTemplateColumns: {
