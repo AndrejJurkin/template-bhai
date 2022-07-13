a13f4cb77cb01edcb2fbc2439e1723b67f4d6c0d
Chigala Enyeazu <chigala11@gmail.com>
2022-07-13 11:39:59 +0100

    added a title, remove arbituary values, added an overlay when the sidebar is drawn in mobile, formatted my files with prettier

--- static/E-commerce-template/main.js
+++ static/E-commerce-template/main.js
@@ -1,30 +1,25 @@
 import "./style.css";
 
-
 const closeSidebar = document.querySelector("#close-sideBar");
 const sidebar = document.querySelector("#sidebar");
 const openSidebar = document.querySelector("#open-sidebar");
 const openSearchModal = document.querySelector("#searchBtn");
 const searchModal = document.querySelector("#search-modal");
+const overlay = document.querySelector("#overlay");
 
 closeSidebar.addEventListener("click", () => {
-  sidebar.classList.remove('translate-x-0');
+  sidebar.classList.remove("translate-x-0");
   sidebar.classList.add("translate-x-full");
-  console.log("the drawer has been closed");
+  overlay.classList.toggle("hidden");
 });
 openSidebar.addEventListener("click", () => {
   sidebar.classList.remove("translate-x-full");
+  overlay.classList.toggle("hidden");
   sidebar.classList.add("translate-x-0");
-  console.log("the the sidebar has opened has opened");
-  
 });
 
 openSearchModal.addEventListener("click", () => {
   searchModal.classList.toggle("nav-items--hidden");
-  
-  console.log("the search modal is opening");
 });
 
-  document.querySelector("#copyright-year").innerText =
-    new Date().getFullYear();
-;
+document.querySelector("#copyright-year").innerText = new Date().getFullYear();
--- static/E-commerce-template/tailwind.config.js
+++ static/E-commerce-template/tailwind.config.js
@@ -3,18 +3,22 @@ module.exports = {
   content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {
-       fontFamily: {
-        'inter': ['inter', 'sans-serif'], 
-        'noto-serif': ['"Noto Serif"', 'serif']
+      fontFamily: {
+        inter: ["inter", "sans-serif"],
+        "noto-serif": ['"Noto Serif"', "serif"],
       },
-    colors: {
-      "primary": "#ED9E2D",
-      "primary-text": "#694A1C",
-      "primary-light": "#F9E5CA",
-      "primary-bold": "#121236",
-      "primary-very-light": "#B3ACA4"
-    },
+      fontSize: {
+        tiny: "9px",
+        "tiny-medium": "11px",
+      },
+      colors: {
+        primary: "#ED9E2D",
+        "primary-text": "#694A1C",
+        "primary-light": "#F9E5CA",
+        "primary-bold": "#121236",
+        "primary-very-light": "#B3ACA4",
+      },
     },
   },
   plugins: [],
-}
+};
