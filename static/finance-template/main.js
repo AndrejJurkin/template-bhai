531bd3c9663dbb5ed52babf2d9dac5b80db1f9f8
Chigala Enyeazu <chigala11@gmail.com>
2022-07-14 15:06:35 +0100

    finished the finance-template

---
+++ static/finance-template/main.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+const menuBtn = document.querySelector("#menu-btn");
+const sidebar = document.querySelector("#sidebar");
+const closeSidebar = document.querySelector("#close-sideBar");
+
+closeSidebar.addEventListener("click", () => {
+  sidebar.classList.remove("translate-x-0");
+  sidebar.classList.add("translate-x-full");
+});
+
+menuBtn.addEventListener("click", () => {
+  sidebar.classList.remove("translate-x-full");
+  sidebar.classList.add("translate-x-0");
+});
---
+++ static/finance-template/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "DM-sans": ["DM Sans", "sans-serif"],
+      },
+      fontSize: {
+        tiny: "9px",
+      },
+      colors: {
+        dim: "#7E8597",
+        "button-color": "#E46138",
+        background: "#141A25",
+      },
+    },
+    plugins: [],
+  },
+};
