15b057007e16b52f0a25616ab75f091fd04799e9
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-15 09:47:51 +0200

    Delete empty divs. Change margin-top to paading-top

--- apps/next_dev_portfolio/components/Projects.js
+++ apps/next_dev_portfolio/components/Projects.js
@@ -3,60 +3,60 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <>
-      <div id="projects"></div>
-      <section className="max-w-4xl px-4 mx-auto mt-32 space-y-8 text-center lg:lg:mt-52">
-        <h2 className="text-4xl text-white">
-          Projects
-          <DotSmall />
-        </h2>
-        <div className="gap-8 xl:grid xl:grid-cols-2">
-          <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">Project tittle</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
-                necessitatibus ab dolorum illo sed consectetur nulla modi
-                officia iure, quis nam similique!
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">Project tittle</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
-                necessitatibus ab dolorum illo sed consectetur nulla modi
-                officia iure, quis nam similique!
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">Project tittle</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
-                necessitatibus ab dolorum illo sed consectetur nulla modi
-                officia iure, quis nam similique!
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
-            <div className="px-6 py-4">
-              <div className="mb-2 text-xl font-bold">Project tittle</div>
-              <p className="mt-4 text-xl text-gray-400 ">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
-                necessitatibus ab dolorum illo sed consectetur nulla modi
-                officia iure, quis nam similique!
-              </p>
-            </div>
+    <section
+      id="projects"
+      className="max-w-4xl px-4 mx-auto pt-32 space-y-8 text-center lg:lg:pt-52"
+    >
+      <h2 className="text-4xl text-white">
+        Projects
+        <DotSmall />
+      </h2>
+      <div className="gap-8 xl:grid xl:grid-cols-2">
+        <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Project tittle</div>
+            <p className="mt-4 text-xl text-gray-400 ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
+              necessitatibus ab dolorum illo sed consectetur nulla modi officia
+              iure, quis nam similique!
+            </p>
           </div>
         </div>
-      </section>
-    </>
+        <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Project tittle</div>
+            <p className="mt-4 text-xl text-gray-400 ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
+              necessitatibus ab dolorum illo sed consectetur nulla modi officia
+              iure, quis nam similique!
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Project tittle</div>
+            <p className="mt-4 text-xl text-gray-400 ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
+              necessitatibus ab dolorum illo sed consectetur nulla modi officia
+              iure, quis nam similique!
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center h-full max-w-xl p-4 mx-auto my-2 overflow-hidden rounded shadow-lg bg-neutral-800">
+          <div className="px-6 py-4">
+            <div className="mb-2 text-xl font-bold">Project tittle</div>
+            <p className="mt-4 text-xl text-gray-400 ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem harum, quisquam totam sunt cumque facilis vel laborum
+              necessitatibus ab dolorum illo sed consectetur nulla modi officia
+              iure, quis nam similique!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- apps/next_dev_portfolio/components/Skills.js
+++ apps/next_dev_portfolio/components/Skills.js
@@ -12,53 +12,53 @@ import Link from "next/link";
 export default function Skills() {
   const [width, height] = [100, 100];
   return (
-    <>
-      <div id="skills"></div>
-      <section className="max-w-4xl px-4 mx-auto mt-32 space-y-8 text-center lg:mt-52">
-        <h2 className="text-4xl text-white">
-          I work with
-          <DotSmall />
-        </h2>
+    <section
+      id="skills"
+      className="max-w-4xl px-4 mx-auto pt-32 space-y-8 text-center lg:pt-52"
+    >
+      <h2 className="text-4xl text-white">
+        I work with
+        <DotSmall />
+      </h2>
 
-        <div className="grid grid-cols-2 gap-4 text-white lg:grid-cols-6 ">
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://reactjs.org/">
-              <a className="mb-2 text-gray-400 link">React</a>
-            </Link>
-            <ReactIcon width={width} height={height} />
-          </p>
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://nextjs.org">
-              <a className="mb-2 text-gray-400 link">Next.js</a>
-            </Link>
-            <NextIcon width={width} height={height} />
-          </p>
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://tailwindcss.com">
-              <a className="mb-2 text-gray-400 link">TailwindCSS</a>
-            </Link>
-            <TailwindIcon width={width} height={height} />
-          </p>
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://mui.com">
-              <a className="mb-2 text-gray-400 link">Material UI</a>
-            </Link>
-            <MUIIcon width={width} height={height} />
-          </p>
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://www.djangoproject.com">
-              <a className="mb-2 text-gray-400 link">Django</a>
-            </Link>
-            <DjangoIcon width={width} height={height} />
-          </p>
-          <p className="flex flex-col items-center justify-center">
-            <Link href="https://www.docker.com">
-              <a className="mb-2 text-gray-400 link">Docker</a>
-            </Link>
-            <DockerIcon width={width} height={height} />
-          </p>
-        </div>
-      </section>
-    </>
+      <div className="grid grid-cols-2 gap-4 text-white lg:grid-cols-6 ">
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://reactjs.org/">
+            <a className="mb-2 text-gray-400 link">React</a>
+          </Link>
+          <ReactIcon width={width} height={height} />
+        </p>
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://nextjs.org">
+            <a className="mb-2 text-gray-400 link">Next.js</a>
+          </Link>
+          <NextIcon width={width} height={height} />
+        </p>
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://tailwindcss.com">
+            <a className="mb-2 text-gray-400 link">TailwindCSS</a>
+          </Link>
+          <TailwindIcon width={width} height={height} />
+        </p>
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://mui.com">
+            <a className="mb-2 text-gray-400 link">Material UI</a>
+          </Link>
+          <MUIIcon width={width} height={height} />
+        </p>
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://www.djangoproject.com">
+            <a className="mb-2 text-gray-400 link">Django</a>
+          </Link>
+          <DjangoIcon width={width} height={height} />
+        </p>
+        <p className="flex flex-col items-center justify-center">
+          <Link href="https://www.docker.com">
+            <a className="mb-2 text-gray-400 link">Docker</a>
+          </Link>
+          <DockerIcon width={width} height={height} />
+        </p>
+      </div>
+    </section>
   );
 }
