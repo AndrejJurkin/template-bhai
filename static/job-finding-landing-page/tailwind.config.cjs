bd410383b773870f17bd28cb094bbf8693d4c8c5
Subho <contact.subhoghosh@gmail.com>
2022-07-22 20:30:42 +0530

    done

--- static/job-finding-landing-page/tailwind.config.cjs
+++ static/job-finding-landing-page/tailwind.config.cjs
@@ -2,9 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {
       colors: {
         bgColor: "#C2E4FF",
+        dGray: "#2D2D2D",
+        cGray: "#4C4C4C",
+        borColor: "#FFF5EC",
+        bgColor2: "#FFDDC2",
+        borColor2: "#E5D7CC",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
