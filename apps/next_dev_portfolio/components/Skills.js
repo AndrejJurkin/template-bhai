0db56c38988791bf9b94c50a2b0f776e1edda971
Maciej Adamski <Maciej.Adamski@kyndryl.com>
2022-07-25 13:25:56 +0200

    Fix dev links repetition

--- apps/next_dev_portfolio/components/Skills.js
+++ apps/next_dev_portfolio/components/Skills.js
@@ -11,11 +11,44 @@ import {
 
 const [width, height] = [100, 100];
 
+const dev_list = [
+  {
+    name: "React",
+    icon: <ReactIcon width={width} height={height} />,
+    link: "https://reactjs.org/",
+  },
+  {
+    name: "Next.js",
+    icon: <NextIcon width={width} height={height} />,
+    link: "https://nextjs.org/",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <TailwindIcon width={width} height={height} />,
+    link: "https://tailwindcss.com/",
+  },
+  {
+    name: "Material UI",
+    icon: <MUIIcon width={width} height={height} />,
+    link: "https://material-ui.com/",
+  },
+  {
+    name: "Docker",
+    icon: <DockerIcon width={width} height={height} />,
+    link: "https://www.docker.com/",
+  },
+  {
+    name: "Django",
+    icon: <DjangoIcon width={width} height={height} />,
+    link: "https://www.djangoproject.com/",
+  },
+];
+
 export default function Skills() {
   return (
     <section
       id="skills"
-      className="max-w-4xl px-4 mx-auto pt-32 space-y-8 text-center lg:pt-52"
+      className="max-w-4xl px-4 pt-32 mx-auto space-y-8 text-center lg:pt-52"
     >
       <h2 className="text-4xl text-white">
         I work with
@@ -23,42 +56,19 @@ export default function Skills() {
       </h2>
 
       <ul className="grid grid-cols-2 gap-4 text-white lg:grid-cols-6 ">
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://reactjs.org/">
-            <a className="mb-2 text-gray-400 link">React</a>
-          </Link>
-          <ReactIcon width={width} height={height} />
-        </li>
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://nextjs.org">
-            <a className="mb-2 text-gray-400 link">Next.js</a>
-          </Link>
-          <NextIcon width={width} height={height} />
-        </li>
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://tailwindcss.com">
-            <a className="mb-2 text-gray-400 link">TailwindCSS</a>
-          </Link>
-          <TailwindIcon width={width} height={height} />
-        </li>
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://mui.com">
-            <a className="mb-2 text-gray-400 link">Material UI</a>
-          </Link>
-          <MUIIcon width={width} height={height} />
-        </li>
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://www.djangoproject.com">
-            <a className="mb-2 text-gray-400 link">Django</a>
-          </Link>
-          <DjangoIcon width={width} height={height} />
-        </li>
-        <li className="flex flex-col items-center justify-center">
-          <Link href="https://www.docker.com">
-            <a className="mb-2 text-gray-400 link">Docker</a>
-          </Link>
-          <DockerIcon width={width} height={height} />
-        </li>
+        {dev_list.map((dev) => {
+          return (
+            <li
+              key={dev.name}
+              className="flex flex-col items-center justify-center"
+            >
+              <Link href={dev.link}>
+                <a className="mb-2 text-gray-400 link">{dev.name}</a>
+              </Link>
+              {dev.icon}
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
