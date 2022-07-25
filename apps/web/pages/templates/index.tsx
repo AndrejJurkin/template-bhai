0a903b329ad6b6b569b1466ca81361530ea53caf
Subho <contact.subhoghosh@gmail.com>
2022-07-26 00:58:52 +0530

    refactor: get the templates data from json

--- apps/web/pages/templates/index.tsx
+++ apps/web/pages/templates/index.tsx
@@ -1,19 +1,23 @@
 import fs from "fs";
 import Link from "next/link";
 import path from "path";
+import getAllTemplates from "../../utils/getAllTemplates";
 
 interface TemplatesProps {
-  paths: string[];
+  templatesData: {
+    name: string;
+    link: string;
+  }[];
 }
 
-export default function Templates({ paths }: TemplatesProps) {
+export default function Templates({ templatesData }: TemplatesProps) {
   return (
     <div className="mt-20 flex flex-col items-center text-center">
       <h1 className="text-3xl font-bold">Templates Page</h1>
       <nav className="mt-8 flex flex-col space-y-1 text-blue-500">
-        {paths.map((path) => (
-          <Link href={`/templates/${path}/`}>
-            <a>{path}</a>
+        {templatesData.map((templateData, index) => (
+          <Link href={templateData.link} key={index}>
+            <a>{templateData.name}</a>
           </Link>
         ))}
       </nav>
@@ -22,20 +26,11 @@ export default function Templates({ paths }: TemplatesProps) {
 }
 
 export async function getStaticProps() {
-  const rawPaths = fs.readdirSync(
-    path.join(__dirname, "../../../../../static")
-  );
-
-  const paths = rawPaths.filter((path) => {
-    if (path.includes(".") || path.includes("_")) {
-      return false;
-    }
-    return true;
-  });
+  const templatesData = await getAllTemplates();
 
   return {
     props: {
-      paths,
+      templatesData,
     },
   };
 }
---
+++ apps/web/utils/getAllTemplates.ts
@@ -0,0 +1,5 @@
+import templateData from './template-data.json';
+
+export default async function getAllTemplates() {
+    return templateData;
+}
