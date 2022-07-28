38c0e1ab61c6f62b70840bf832bc16b8e632455f
Karthik\Karthik S K <sk.karthik315@gmail.com>
2022-07-28 09:50:05 +0530

    Added Config for Super Linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,27 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+
+  extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+    node: true,
+  },
+
+  rules: {
+    "no-undef": "warn",
+    quotes: [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    semi: ["error", "always"],
+    "comma-dangle": "warn",
+  },
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
   settings: {
     next: {
       rootDir: ["apps/*/"],
