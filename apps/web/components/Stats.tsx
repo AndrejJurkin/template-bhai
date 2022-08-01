1d1e17bc5a16bee763fc589e99c2fb1964c059f1
Subho <contact.subhoghosh@gmail.com>
2022-08-01 21:41:41 +0530

    refactor: make everything div

--- apps/web/components/Stats.tsx
+++ apps/web/components/Stats.tsx
@@ -8,30 +8,28 @@ export default function Stats({ stats }: StatsProps) {
   return (
     <section className="py-14">
       <div className="container grid grid-cols-2 gap-y-9 gap-x-12 text-center sm:grid-cols-4">
-        <p className="space-y-1 xl:space-y-2">
-          <span className="block text-4xl font-bold xl:text-5xl">
+        <div className="space-y-1 xl:space-y-2">
+          <div className="text-4xl font-bold xl:text-5xl">
             {stats.templates}
-          </span>
-          <span className="block text-slate-400 xl:text-xl">Templates</span>
-        </p>
-        <p className="space-y-1 xl:space-y-2">
-          <span className="block text-4xl font-bold xl:text-5xl">
+          </div>
+          <div className="text-slate-400 xl:text-xl">Templates</div>
+        </div>
+        <div className="space-y-1 xl:space-y-2">
+          <div className="text-4xl font-bold xl:text-5xl">
             {stats.contributors}
-          </span>
-          <span className="block text-slate-400 xl:text-xl">Contributors</span>
-        </p>
-        <p className="space-y-1 xl:space-y-2">
-          <span className="block text-4xl font-bold xl:text-5xl">
-            {stats.commits}+
-          </span>
-          <span className="block text-slate-400 xl:text-xl">Commits</span>
-        </p>
-        <p className="space-y-1 xl:space-y-2">
-          <span className="block text-4xl font-bold xl:text-5xl">
+          </div>
+          <div className="text-slate-400 xl:text-xl">Contributors</div>
+        </div>
+        <div className="space-y-1 xl:space-y-2">
+          <div className="text-4xl font-bold xl:text-5xl">{stats.commits}+</div>
+          <div className="text-slate-400 xl:text-xl">Commits</div>
+        </div>
+        <div className="space-y-1 xl:space-y-2">
+          <div className="text-4xl font-bold xl:text-5xl">
             {stats.githubStars}
-          </span>
-          <span className="block text-slate-400 xl:text-xl">GitHub Stars</span>
-        </p>
+          </div>
+          <div className="text-slate-400 xl:text-xl">GitHub Stars</div>
+        </div>
       </div>
     </section>
   );
