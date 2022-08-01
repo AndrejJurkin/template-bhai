37faa0a0e59f71250e3b61e70a01fee4941497d5
Subho <contact.subhoghosh@gmail.com>
2022-08-01 21:37:38 +0530

    refactor: move icons to a separate assests file

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useState } from "react";
+import { CloseIcon, HamburgerIcon } from "../assets/icons";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -22,34 +23,12 @@ export default function Header() {
               className="md:hidden"
               onClick={() => setMenuOpen((state) => !state)}
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-6 w-6 ${menuOpen ? "hidden" : "block"}`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`h-6 w-6 ${!menuOpen ? "hidden" : "block"}`}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <span className={`${menuOpen ? "hidden" : "block"}`}>
+                {HamburgerIcon}
+              </span>
+              <span className={`${!menuOpen ? "hidden" : "block"}`}>
+                {CloseIcon}
+              </span>
             </button>
           </div>
 
