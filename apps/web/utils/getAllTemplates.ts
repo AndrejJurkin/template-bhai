bc607292cbdec4517ecb1631669710b98d62dd59
Subho <contact.subhoghosh@gmail.com>
2022-08-02 00:27:33 +0530

    add a src key in template-data json

--- apps/web/utils/getAllTemplates.ts
+++ apps/web/utils/getAllTemplates.ts
@@ -3,6 +3,7 @@ import templatesData from "./template-data.json";
 export interface Template {
   name: string;
   link: string;
+  src: string;
 }
 
 export default async function getAllTemplates() {
